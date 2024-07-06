08bc9cadd56a62391aa01cd5fbe3d1109466e5eb
mahdi7716 <32459776+mahdi7716@users.noreply.github.com>
2024-07-06 19:56:40 +0330

    create ui payment - test

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,11 +1,14 @@
 import { permanentRedirect } from "next/navigation";
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers';
+import { deleteCookie } from 'cookies-next';
 
 export async function POST(req, res) {
     // const response = NextResponse.next();
     // console.log("response--->", res)
     // console.log("req--->", req)
-     res.cookies.delete('paymentData')
+    deleteCookie('paymentData', { req, res });
+    // cookies.delete('paymentData')
     permanentRedirect(`https://peyvand.razaviedu.ir/p-lecturer`)
     // // return NextResponse.redirect(new URL(`http://localhost:3000`, req.url));
 
